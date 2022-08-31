7654b9d9444dab4c59afebc94ecf052ddc8b6d37
TJ Hindman <thind131@gmail.com>
2022-08-31 13:20:35 -0400

    Projects component basically completed

---
+++ src/assets/projects.js
@@ -0,0 +1,8 @@
+// ! had to refactor from ".json" to ".js" so I could include "img" pathway properly
+export const projArr = [
+    {
+        name: "Fresh Kutz Barbershop",
+        url: "https://freshkutzindy.com/",
+        img: require("./fresh-kutz-web.png")
+    }
+]
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,29 @@
 import React from "react";
-import projArr from "../assets/projects.json";
+import { projArr } from "../assets/projects.js";
 
 const Projects = () => {
   return (
-    <div className="flex flex-col w-[100%] h-[50vh]" id="projects">
+    <div className="flex flex-col w-[100%]" id="projects">
       <h1 className="self-start font-bold text-3xl my-8 md:my-20 md:text-5xl">
         Projects
       </h1>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center max-h-[36vh] md:max-h-[43vh] overflow-y-auto">
         {projArr.map((proj) => (
-          <a className="w-[90%]" target="_blank" href={proj.url}>
-            <button className="rounded-[10px]">{proj.name}</button>
+          <a className="w-[90%] my-2" href={proj.url} target="_blank">
+            <button className="flex justify-between items-center rounded-[10px] transition ease-in delay-150 hover:-translate-y-2 duration-400">
+              <img className="w-[20%]" src={proj.img} alt={proj.name} />
+              <div className="w-[80%] text-center">{proj.name}</div>
+            </button>
           </a>
         ))}
       </div>
-      <a className="self-end" href="">Check out my Github for more code!</a>
+      <a
+        className="self-end my-8 underline hover:text-blue-400 active:text-blue-900 visited:text-blue-600"
+        href="https://github.com/tjhindman"
+        target="_blank"
+      >
+        Check out my Github for more code!
+      </a>
     </div>
   );
 };
